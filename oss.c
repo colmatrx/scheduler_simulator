3980972b3e4f355071e658ffa1b5861c58559f92
colmatrx <55037320+colmatrx@users.noreply.github.com>
2021-11-01 16:20:16 -0500

    Add files via upload

--- oss.c
+++ oss.c
@@ -15,6 +15,8 @@
 
 void timeouthandler(int sig);   //timeout handler function declaration
 
+void siginthandler(int);
+
 void initclock(void);   //function to initialize the two clocks in shared memory
 
 void cleanup(void);
@@ -27,6 +29,7 @@ char *logfilename; // = "mess"; to save the log file name passed on the cmd line
 
 int ossclockid, processtableid; unsigned int *ossclockaddress, *osstimeseconds, *osstimenanoseconds;
 int messageqid; char logstring[1024]; int processcount = 0;
+int ossofflinesecondclock, ossofflinenanosecondclock;
 
 struct msg {
 
@@ -47,15 +50,15 @@ typedef struct processes{
 } process;
 
 process userprocess[18]; //Array representing Process Control Tab
-process *processtableaddress;
+process *processtableaddress; //pointer address to traverse the process control blocks
 
 int main(int argc, char *argv[]){
 
     int option; //for tracking the command line argument provided
     int oss_run_timeout = oss_wait_timeout; //time before oss times out and kill all processes and exit. should be handled in signal handler
-    long int userprocessid; int ossofflinesecondclock, ossofflinenanosecondclock;
+    long int userprocessid;
 
-    processtableid = shmget(processtablekey, (sizeof(process)*18), IPC_CREAT|0766); //create a shared memory 
+    processtableid = shmget(processtablekey, (sizeof(process)*18), IPC_CREAT|0766); //create a shared memory allocation for 18 processes
 
     ossclockid = shmget(ossclockkey, 8, IPC_CREAT|0766);//getting the oss clock shared memory clock before initializing it so that the id can become available to the child processes
     if (ossclockid == -1){  //checking if shared memory id was successfully created
@@ -168,7 +171,7 @@ int main(int argc, char *argv[]){
         usermessage.msgcontent = runtime; usermessage.msgtype = getpid(); //sets the usermessage before sending it to OSS
         msgsnd(messageid, &usermessage, sizeof(usermessage), IPC_NOWAIT); //sends a message back to OSS
 
-        if ((shmdt(proctableaddress)) == -1){    //detaching from the process table shared memory
+        if ((shmdt(proctableaddress)) == -1){    //detaching from the process table shared memory after readig from it
 
             perror("\noss: Error: In user process() section, process table shmdt()) failed. process table shared memory cannot be detached");
             exit(1);
@@ -176,6 +179,8 @@ int main(int argc, char *argv[]){
         
         return 0;
     }
+
+    signal(SIGINT, siginthandler); //handles Ctrl+C signal inside OSS only
 
     processcount = processcount + 1; //incrememt to track the total processes in the system; maximum allowed is 18
 
@@ -197,7 +202,7 @@ int main(int argc, char *argv[]){
 
     printf("\nOSS process ID is %d\n", getpid());
 
-    snprintf(logstring, sizeof(logstring), "\nOSS: User Process %d created at time %d second and %d nanosecond", (processtableaddress+(processcount-1))->processid, *osstimeseconds, *osstimenanoseconds);
+    snprintf(logstring, sizeof(logstring), "\n\nOSS: User Process %d created at time %d second and %d nanosecond", (processtableaddress+(processcount-1))->processid, *osstimeseconds, *osstimenanoseconds);
 
     logmsg(logfilename, logstring); //write logstring above to log file
 
@@ -220,51 +225,7 @@ int main(int argc, char *argv[]){
 
     //Clean Up Block....Message queues and Shared Memory are freed here
 
-    cleanup(); //called to free up Message Queue
-
-    if ((shmdt(processtableaddress)) == -1){    //detaching from the process table shared memory
-
-        perror("\noss: Error: In cleanup() section, process table shmdt()) failed. process table shared memory cannot be detached");
-        exit(1);
-    }
-
-    printf("\nprocess table shared memory was detached.\n");
-
-    if (shmctl(processtableid, IPC_RMID, NULL) != 0){      //shmctl() marks the oss process table shared memory for destruction so it can be deallocated from memory after no process is using it
-        perror("\noss: Error: In cleanup() section, process table shmctl() call failed. Segment cannot be marked for destruction\n"); //error checking shmctl() call
-        exit(1);
-    }
-
-    printf("\nprocess table shared memory was deleted.\n\n");
-
-    snprintf(logstring, sizeof(logstring), "\nOSS: Process Table Shared Memory ID %d has been detached and deleted at time %d seconds and %d nanoseconds.", processtableid, *osstimeseconds, *osstimenanoseconds);
-    logmsg(logfilename, logstring);
-
-    *osstimeseconds = *osstimeseconds + 1; //increment oss clock by 1 second
-
-    ossofflinesecondclock = *osstimeseconds;
-    ossofflinenanosecondclock = *osstimenanoseconds;
-
-    snprintf(logstring, sizeof(logstring), "\nOSS: OSS Clock Shared Memory ID %d has been detached and deleted at time %d seconds and %d nanoseconds.", ossclockid, *osstimeseconds, *osstimenanoseconds);
-    logmsg(logfilename, logstring);
-
-    if ((shmdt(ossclockaddress)) == -1){    //detaching from the oss clock shared memory
-
-        perror("\noss: Error: In cleanup() section, shmdt()) failed. OSS clock address shared memory cannot be detached");
-        exit(1);
-    }
-
-    printf("\nOSS clock shared memory was detached.\n");
-
-    if (shmctl(ossclockid, IPC_RMID, NULL) != 0){      //shmctl() marks the oss clock shared memory for destruction so it can be deallocated from memory after no process is using it
-        perror("\noss: Error: In cleanup() section, OSS clockid shmctl() call failed. Segment cannot be marked for destruction\n"); //error checking shmctl() call
-        exit(1);
-    }
-
-    printf("\nOSS clock shared memory was deleted.\n\n");
-
-    snprintf(logstring, sizeof(logstring), "\nOSS: OSS suucessfully completed execution at %d seconds and %d nanoseconds\n", ossofflinesecondclock + 1, ossofflinenanosecondclock);
-    logmsg(logfilename, logstring);
+    cleanup(); //called to free up Message Queue and Delete Share Memories
 
     return 0;
 
@@ -274,13 +235,62 @@ int main(int argc, char *argv[]){
 
 void timeouthandler(int sig){   //this function is called if the program times out after oss_run_timeout seconds. Handle killing child processes and freeing resources in here later
 
-    printf("\noss: Error: System Timeout: In timeout handler. Aborting Child and Parent Processes..\n");
+    printf("\noss: Error: System Timeout. In timeout handler. Aborting Child and Parent Processes..\n");
 
-    logmsg(logfilename, "\nOSS Times out at getTime() from scheduler");
+    *osstimeseconds = *osstimeseconds + 180; //timeout happens after 3 minutes
+
+    snprintf(logstring, sizeof(logstring), "\nOSS: System Timeout at %d seconds and %d nanoseconds. In Timeout Signal Handler. Aborting User Processes and Cleaning up resources...", *osstimeseconds, *osstimenanoseconds);
+    logmsg(logfilename, logstring);
+
+    for (int i = 0; i < 18; i++){       //traverse the process table to locate and kill user processes
+            if (((processtableaddress+i)->processid) > 0){
+
+                kill((processtableaddress+i)->processid, SIGKILL); //kills any process with pid greater than 0
+                snprintf(logstring, sizeof(logstring), "\nOSS: User Process %d terminated", (processtableaddress+i)->processid);
+                logmsg(logfilename, logstring);
+
+            }
+    }
+
+    *osstimeseconds = *osstimeseconds + 1; *osstimenanoseconds = *osstimenanoseconds + 15; //increment clock before calling cleanup()
+
+    cleanup(); //call cleanup to free up message queue and delete shared memories
+
+    snprintf(logstring, sizeof(logstring), "\nOSS: OSS terminating at time %d seconds and %d nanseconds.", ossofflinesecondclock, ossofflinenanosecondclock+5);
+    logmsg(logfilename, logstring);
+
+    kill(getpid(), SIGTERM);
 
     exit(1);
 
-    }
+}
+
+void siginthandler(int sigint){
+
+        printf("\noss: Error: Ctrl+C received. In siginthandler() handler. Aborting User Processes..\n");
+
+        snprintf(logstring, sizeof(logstring), "\nOSS: Error!: Ctrl+C interrupt was received at time %d seconds and %d nanoseconds. Aborting User Processes...",*osstimeseconds, *osstimenanoseconds);
+        logmsg(logfilename, logstring);
+
+        for (int i = 0; i < 18; i++){       //traverse the process table to locate and kill user processes
+            if (((processtableaddress+i)->processid) > 0){
+
+                kill((processtableaddress+i)->processid, SIGKILL); //kills any process with pid greater than 0
+                snprintf(logstring, sizeof(logstring), "\nOSS: User Process %d terminated", (processtableaddress+i)->processid);
+                logmsg(logfilename, logstring);
+
+            }
+        }
+
+        cleanup();
+
+        snprintf(logstring, sizeof(logstring), "\nOSS: OSS terminating at time %d seconds and %d nanseconds.", ossofflinesecondclock, ossofflinenanosecondclock+5);
+        logmsg(logfilename, logstring);
+
+        kill(getpid(), SIGTERM);
+
+        exit(1);
+}
 
 void initclock(void){ //initializes the seconds and nanoseconds parts of the oss
 
@@ -295,7 +305,7 @@ void initclock(void){ //initializes the seconds and nanoseconds parts of the oss
     osstimeseconds = ossclockaddress + 0;   //the first 4 bytes of the address stores the seconds part of the oss clock, note the total address space is for 8 bytes from shmget above
     osstimenanoseconds = ossclockaddress + 1;   //the second 4 bytes of the address stores the seconds part of the oss clock
 
-    *osstimeseconds = 0;    //storing integer data in the seonds part of the oss clock
+    *osstimeseconds = 0;    //storing integer data in the seconds part of the oss clock
     *osstimenanoseconds = 0;    //storing int data in the nanoseconds part of the oss clock
 
 }
@@ -317,7 +327,7 @@ void cleanup(void){ //cleans up the message queue
 
         printf("\nMessage Queue ID %d has been removed.\n", messageqid);
 
-        snprintf(logstring, sizeof(logstring), "\nOSS: Message Queue ID %d has been removed at time %d seconds and %d nanoseconds.", messageqid, *osstimeseconds, *osstimenanoseconds);
+        snprintf(logstring, sizeof(logstring), "\nOSS: Cleaning up used resources...\nOSS: Message Queue ID %d has been removed at time %d seconds and %d nanoseconds.", messageqid, *osstimeseconds, *osstimenanoseconds);
         logmsg(logfilename, logstring);
         *osstimenanoseconds = *osstimenanoseconds + 15;
     }
@@ -326,5 +336,47 @@ void cleanup(void){ //cleans up the message queue
         printf("\noss: Error: In cleanup(), Message Queue removal failed!\n\n");
         exit(1);
     }
+
+    //Now detach and delete shared memories
+
+     if ((shmdt(processtableaddress)) == -1){    //detaching from the process table shared memory
+
+        perror("\noss: Error: In cleanup() section, process table shmdt()) failed. process table shared memory cannot be detached");
+        exit(1);
+    }
+
+    printf("\nprocess table shared memory was detached.\n");
+
+    if (shmctl(processtableid, IPC_RMID, NULL) != 0){      //shmctl() marks the oss process table shared memory for destruction so it can be deallocated from memory after no process is using it
+        perror("\noss: Error: In cleanup() section, process table shmctl() call failed. Segment cannot be marked for destruction\n"); //error checking shmctl() call
+        exit(1);
+    }
+
+    printf("\nprocess table shared memory was deleted.\n\n");
+
+    snprintf(logstring, sizeof(logstring), "\nOSS: Process Table Shared Memory ID %d has been detached and deleted at time %d seconds and %d nanoseconds.", processtableid, *osstimeseconds, *osstimenanoseconds);
+    logmsg(logfilename, logstring);
+
+    ossofflinesecondclock = *osstimeseconds; ossofflinenanosecondclock = *osstimenanoseconds; //save the clock before detaching from clock shared memory
+
+    if ((shmdt(ossclockaddress)) == -1){    //detaching from the oss clock shared memory
+
+        perror("\noss: Error: In cleanup() section, shmdt()) failed. OSS clock address shared memory cannot be detached");
+        exit(1);
+    }
+
+    printf("\nOSS clock shared memory was detached.\n");
+
+    if (shmctl(ossclockid, IPC_RMID, NULL) != 0){      //shmctl() marks the oss clock shared memory for destruction so it can be deallocated from memory after no process is using it
+        perror("\noss: Error: In cleanup() section, OSS clockid shmctl() call failed. Segment cannot be marked for destruction\n"); //error checking shmctl() call
+        exit(1);
+    }
+
+    printf("\nOSS clock shared memory was deleted.\n\n");
+
+    ossofflinesecondclock = ossofflinesecondclock + 1; ossofflinenanosecondclock = ossofflinenanosecondclock + 15;
+
+    snprintf(logstring, sizeof(logstring), "\nOSS: OSS Clock Shared Memory ID %d has been detached and deleted at time %d seconds and %d nanoseconds.", ossclockid, ossofflinesecondclock, ossofflinenanosecondclock);
+    logmsg(logfilename, logstring);
 
 }
